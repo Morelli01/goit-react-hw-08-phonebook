2dd81ab1fa5981bf031cbfcbd35b6eccfdd18973
Morelli01 <bkmz055@ukr.net>
2023-08-18 01:15:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Layout>
-          <BrowserRouter basename="goit-react-hw-08-phonebook">
+          <BrowserRouter basename="/Morelli01/goit-react-hw-08-phonebook">
             <App />
           </BrowserRouter>
         </Layout>
